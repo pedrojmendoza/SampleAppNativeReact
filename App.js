40b017b2b2e3ff7f3056bee4558af0f941848845
Mendoza <menpedro@amazon.com>
2018-03-22 14:17:24 +0100

    ejecting CRNA and using tcomb-form

--- App.js
+++ App.js
@@ -1,14 +1,73 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import t from 'tcomb-form-native'; // 0.6.9
 
-export default class App extends React.Component {
+const Form = t.form.Form;
+
+const User = t.struct({
+  email: t.String,
+  username: t.maybe(t.String),
+  password: t.String,
+  terms: t.Boolean
+});
+
+const formStyles = {
+  ...Form.stylesheet,
+  formGroup: {
+    normal: {
+      marginBottom: 10
+    },
+  },
+  controlLabel: {
+    normal: {
+      color: 'blue',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    },
+    // the style applied when a validation error occours
+    error: {
+      color: 'red',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    }
+  }
+}
+
+const options = {
+  fields: {
+    email: {
+      error: 'Without an email address how are you going to reset your password when you forget it?'
+    },
+    password: {
+      error: 'Choose something you use on a dozen other sites or something you won\'t remember'
+    },
+    terms: {
+      label: 'Agree to Terms',
+    },
+  },
+  stylesheet: formStyles,
+};
+
+export default class App extends Component {
+  handleSubmit = () => {
+    const value = this._form.getValue();
+    console.log('value: ', value);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>This is a new text being added ...</Text>
+        <Form
+          ref={c => this._form = c}
+          type={User}
+          options={options}
+        />
+        <Button
+          title="Sign Up!"
+          onPress={this.handleSubmit}
+        />
       </View>
     );
   }
@@ -16,9 +75,9 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
   },
 });
